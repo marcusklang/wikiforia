4b637d722719115ae5bdbabee817c1d7a154caa3
Marcus Klang <marcus.klang@cs.lth.se>
2015-02-26 14:30:43 +0100

    * Changed english config

--- src/main/java/se/lth/cs/nlp/wikipedia/lang/EnglishConfig.java
+++ src/main/java/se/lth/cs/nlp/wikipedia/lang/EnglishConfig.java
@@ -16,6 +16,13 @@
  */
 package se.lth.cs.nlp.wikipedia.lang;
 
+import org.apache.commons.lang.StringUtils;
+import se.lth.cs.nlp.mediawiki.model.Page;
+import se.lth.cs.nlp.wikipedia.WikipediaPageType;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * English Wikipedia configuration
  */
@@ -28,6 +35,61 @@ public class EnglishConfig extends TemplateConfig {
     @Override
     protected String getWikiUrl() {
         return "http://en.wikipedia.org/";
+    }
+
+    private static final Pattern stubTextPattern = Pattern.compile("\\{\\{\\s*?(.*?stub)\\s*?\\}\\}", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+    private static final Pattern disambiguationTextPattern = Pattern.compile("\\{\\{\\.*?(disambiguation|disambig|disamb|geodis|hndis|dab)\\.*?\\}\\}", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+    private static final Pattern listTitlePattern = Pattern.compile("(^list\\s+of)", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE | Pattern.DOTALL);
+    private static final Pattern disambiguationTitlePattern = Pattern.compile(".*?\\((\\s)*disambiguation(\\s)*\\)", Pattern.CASE_INSENSITIVE);
+    private static final Pattern templateContent = Pattern.compile("\\{\\{(.+)?\\}\\}");
+
+    private static final String[] disambiguationParts = {"disambiguation", "disambig", "disamb", "geodis", "hndis", "dab"};
+
+    public static boolean matchDisambiguation(String text) {
+        Matcher matcher = templateContent.matcher(text);
+        while(matcher.find()) {
+            String templateContent = matcher.group(1).toLowerCase();
+            for (String disambiguationPart : disambiguationParts) {
+                if(templateContent.contains(disambiguationPart))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean matchStub(String text) {
+        Matcher matcher = templateContent.matcher(text);
+        while(matcher.find()) {
+            String templateContent = matcher.group(1);
+            if(StringUtils.containsIgnoreCase("stub", text))
+                return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public WikipediaPageType classifyPageType(Page page) {
+        WikipediaPageType type = super.classifyPageType(page);
+        if(type == WikipediaPageType.ARTICLE) {
+            //Matcher matcher = disambiguationTextPattern.matcher(page.getContent());
+            if(matchDisambiguation(page.getContent()) || disambiguationTitlePattern.matcher(page.getTitle()).find() ) {
+                return WikipediaPageType.DISAMBIGUATION;
+            }
+
+            //Matcher matcher = stubTextPattern.matcher(page.getContent());
+            if(matchStub(page.getContent())) {
+                return WikipediaPageType.STUB;
+            }
+
+            if(listTitlePattern.matcher(page.getTitle()).find())
+                return WikipediaPageType.LIST;
+
+            return WikipediaPageType.ARTICLE;
+        }
+        else
+            return type;
     }
 
     @Override
